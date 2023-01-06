aa9d0f678538b6e05fc8c077324bda57550cd9a1
umesh <umesh.maurya@neosoftmail.com>
2023-01-06 15:34:04 +0530

    configcommit

--- UserService/src/main/java/com/lcwd/user/service/config/MyConfig.java
+++ UserService/src/main/java/com/lcwd/user/service/config/MyConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.client.RestTemplate;
 public class MyConfig {
 	
 	@Bean
-	@LoadBalanced
+//	@LoadBalanced
 	public RestTemplate restTemplate(){
 		return new RestTemplate();
 	}
